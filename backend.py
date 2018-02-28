373712df35eea5b14df176179f7f0aa2a3348f22
vv20 <vv20@st-andrews.ac.uk>
2018-02-28 20:18:01 +0000

    wrote drum machine logic

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import numpy
 
 from keyboard import Keyboard
 from control import Control
+from drummachine import DrumMachine
 
 class Backend:
     def __init__(self):
@@ -12,10 +13,12 @@ class Backend:
         self.keyboard_midi = self.client.midi_outports.register("keyboard_out")
         self.sampler_midi = self.client.midi_outports.register("sampler_out")
         self.control_midi = self.client.midi_outports.register("control_out")
+        self.drum_machine_midi = self.client.midi_outports.register("drum_machine_out")
         # entities
         self.keyboard = Keyboard(self.keyboard_midi)
         self.sampler = Keyboard(self.sampler_midi)
         self.control = Control(self.control_midi)
+        self.drum_machine = DrumMachine(self.control_midi, self.client.samplerate)
         # callbacks
         self.client.set_shutdown_callback(self.shutdown)
         self.client.set_process_callback(self.process)
@@ -30,3 +33,4 @@ class Backend:
         self.keyboard.process()
         self.sampler.process()
         self.control.process()
+        self.drum_machine.process(no_frames)
